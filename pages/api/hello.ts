e607ee53916c915b0abf4c48fd607b7c14ca222f
Igor Gassmann <igor@igassmann.me>
2022-10-06 16:31:35 +0300

    Set up Sentry and next-superjson-plugin

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,7 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import { withSentry } from "@sentry/nextjs";
 
-type Data = {
-  name: string
-}
+const handler = async () => {
+  throw new Error("API throw error test")
+};
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
-}
+export default withSentry(handler);
